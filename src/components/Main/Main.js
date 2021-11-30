a0437f3df2588ba8a6a16134bb225e83e2888662
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2021-11-30 15:15:16 -0800

    Finished zodiac

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,10 +2,11 @@ import React from 'react'
 import './Main.css'
 import { zodiac } from '../../data'
 import Zodiac from '../Zodiac/Zodiac'
+import background from '../../background.png'
 
 export default function Main() {
   return (
-    <main>
+    <main className="background" style={{ backgroundImage: `url(${background})` }}>
       {zodiac.map((zodi) => (
         <Zodiac key={zodi.name} {...zodi} />
       ))}
--- src/components/Zodiac/Zodiac.js
+++ src/components/Zodiac/Zodiac.js
@@ -3,10 +3,10 @@ import './Zodiac.css'
 
 export default function Zodiac({ name, dates }) {
   return (
-    <div className="zodiac">
+    <div className="zodiac-card">
       <img alt={name} src={`${process.env.PUBLIC_URL}/Zodiac/${name}.png`} />
-      <span className="name">{name}</span>
-      <span>{dates}</span>
+      <div className="name">{name}</div>
+      <div>{dates}</div>
     </div>
   )
 }
